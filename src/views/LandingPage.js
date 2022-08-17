0f75ecea5a93f0b46eaf51444dd6e376bd3ccd94
Shawn-Patrick Bland <shawnpatrick.bland@gmail.com>
2022-08-16 21:32:52 -0600

    two commit

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
-  return (
-    <div>
-      <h1>LandingPage</h1>
-    </div>
-  );
+	return (
+		<Link to='/user/dashboard'>
+			<button>Dashboard</button>
+		</Link>
+	);
 };
 
 export default LandingPage;
